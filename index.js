b8260037a11875a2388f4624174e23466eba5baf
John Pena <jpena91@gmail.com>
2018-12-28 02:10:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-  function theBeatlesPlay(musicians, instruments) {
+function theBeatlesPlay(musicians, instruments) {
   
   var emptyarray = [];
   
-  var arraylength = musicians.length;
-    
-    for (var i = 0; i < arraylength; i++)
-  
-    {emptyarray.push(musicians[i] + " plays " + instruments[i]);
-    
+    for (var i = 0; i < musicians.length; i++)
+    {
+       emptyarray.push(musicians[i]+ " plays "+ instruments[i]);
+    }
+    return emptyarray;
+
 }
+
+function johnLennonFacts(facts)
+{
+ var empty = [];
+ var  i =0 ;
+
+ while (i < facts.length)
+ 
+ {
+   empty.push(facts[i] + "!!!");
+  
+   i++;
+ }
+ return empty;
+}
+
+function iLoveTheBeatles(n){
+  
+  var emptyarray = [];
+  
+  do {emptyarray.push('I love the Beatles!')
+    n++;
+  }
+while (n < 15)
 
   return emptyarray;
   
-}
-
-function johnLennonFacts (facts) {
-  var empty = [];
-var i = 0;
-  while (i < facts.length) { 
-    empty.push(`${facts[i]}!!!`);
-   
-    i++;
-  }
-   
-  return empty; 
-
-}
-
-function iLoveTheBeatles(n) {
-  
-  var vacantarray = [];
-    
-   do {
-      vacantarray.push("I love the Beatles!");
-    n++;
-  }
-    while (n < 15);
-    
-    return vacantarray;
 }
